1704abe9a5f013c48d1125f4b0d48fc93e1dc9a3
sagitovtagir <65604283+sagitovtagir@users.noreply.github.com>
2020-05-23 01:11:46 +0600

    Добавил хлебные крошки

--- myproject/mysite/views.py
+++ myproject/mysite/views.py
@@ -24,6 +24,8 @@ def category(request):
             current_category = category.get(pk=category_int)
             product = product.filter(category__in=current_category.get_descendants(include_self=True))
             context['current_category_name'] = current_category.name
+            current_category_ancestors = current_category.get_ancestors(include_self=True)
+            context['current_category_ancestors'] = current_category_ancestors
             current_category_children = current_category.get_children()
             context['current_category_children'] = current_category_children
         except:
@@ -42,6 +44,9 @@ def product(request):
             product_int = int(product_id)
             product = models.Product.objects.get(pk=product_int)
             context['product'] = product
+            current_category = models.Category.objects.get(pk=product.category_id)
+            current_category_ancestors = current_category.get_ancestors(include_self=True)
+            context['current_category_ancestors'] = current_category_ancestors
             product_image = models.ProductImage.objects.all().filter(product=product)
             context['product_image'] = product_image
         except:
